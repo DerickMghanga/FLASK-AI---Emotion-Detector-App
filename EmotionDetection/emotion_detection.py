19f48524a5bd4e9a8fa3183ff5265f70d5f95bf4
DerickMghanga <derickmghanga3329@gmail.com>
2024-07-01 20:16:04 -0400

    all done

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,20 +8,30 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = myobj, headers=header)
     #print(response)
     formatedResponse = json.loads(response.text)
-    #emotions dict
-    emotions = formatedResponse['emotionPredictions'][0]['emotion']
-    #emotions
-    anger = formatedResponse['emotionPredictions'][0]['emotion']['anger']
-    disgust = formatedResponse['emotionPredictions'][0]['emotion']['disgust']
-    fear = formatedResponse['emotionPredictions'][0]['emotion']['fear']
-    joy = formatedResponse['emotionPredictions'][0]['emotion']['joy']
-    sadness = formatedResponse['emotionPredictions'][0]['emotion']['sadness']
-    dominant = max([anger, disgust, fear, joy, sadness])
+    #error handling
+    if response.status_code == 200:
+        #emotions dict
+        emotions = formatedResponse['emotionPredictions'][0]['emotion']
+        #emotions
+        anger = formatedResponse['emotionPredictions'][0]['emotion']['anger']
+        disgust = formatedResponse['emotionPredictions'][0]['emotion']['disgust']
+        fear = formatedResponse['emotionPredictions'][0]['emotion']['fear']
+        joy = formatedResponse['emotionPredictions'][0]['emotion']['joy']
+        sadness = formatedResponse['emotionPredictions'][0]['emotion']['sadness']
+        dominant = max([anger, disgust, fear, joy, sadness])
 
-    dominant_emotion = ''
-    for key, value in emotions.items():
-        if emotions[key] == dominant:
-            dominant_emotion = key
+        dominant_emotion = ''
+        for key, value in emotions.items():
+            if emotions[key] == dominant:
+                dominant_emotion = key
+    
+    elif response.status_code == 400:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+        dominant_emotion = None
 
     return {'anger':anger, 'disgust':disgust, 'fear':fear, 'joy':joy, 
     'sadness':sadness, 'dominant_emotion': dominant_emotion}
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+''' 
+Application for emotion detector  and deployed on localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -17,9 +20,10 @@ def emotion_detection():
     joy = response['joy']
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+    return f"'anger':{anger},'disgust':{disgust},'fear':{fear},'joy':{joy},'sadness':{sadness}. Dominant {dominant_emotion}."
 
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
-    
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
@@ -28,6 +32,4 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(host="0.0.0.0", port=5000)
